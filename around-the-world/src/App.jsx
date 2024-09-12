ff54aaaa87bb7b44278910474b954d86780dfaa4
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-09-12 23:00:06 +0300

    Initial commit

---
+++ around-the-world/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Logo from "./components/Logo";
+import ThemeSwithcer from "./components/ThemeSwithcer";
+
+function App() {
+  return (
+    <div className="font-inter">
+      <Logo />
+      <ThemeSwithcer />
+    </div>
+  );
+}
+
+export default App;
