d94f9afc8156f1e0e3e19af66531973f505bbd92
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-09-13 17:25:23 +0300

    "Added SearchInput component "

--- around-the-world/src/App.jsx
+++ around-the-world/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import SearchInput from "./components/SearchInput";
 
 function App() {
   return (
     <div className="font-inter dark:text-gray-100">
       <Header />
+      <SearchInput />
     </div>
   );
 }
